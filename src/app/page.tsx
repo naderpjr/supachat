b7209b9e18af225ea7c30bd474b7fb33e7bf821f
Nader PJR <pjrnader@gmail.com>
2025-12-27 21:41:06 +0430

    Home page No chat Room

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyMedia, EmptyTitle } from "@/components/ui/empty";
+import { MessagesSquareIcon } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
-  return null
+  return <div className="container mx-auto max-w-3xl px-4 py-8 space-y-8">
+    <Empty className="border border-dashed">
+      <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <MessagesSquareIcon />
+        </EmptyMedia>
+        <EmptyTitle>No Chat Rooms</EmptyTitle>
+        <EmptyDescription>
+          Create a new chat room to get started.
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
+        <Button asChild>
+          <Link href="rooms/new">Create Room</Link>
+        </Button>
+      </EmptyContent>
+    </Empty>
+  </div>
 }
