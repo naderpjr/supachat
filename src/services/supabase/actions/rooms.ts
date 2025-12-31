88fcc8939e0c5c45afc72b056aa1728cb0c1f866
Nader PJR <pjrnader@gmail.com>
2025-12-31 22:07:45 +0430

    getCurrentUser added

--- src/services/supabase/actions/rooms.ts
+++ src/services/supabase/actions/rooms.ts
@@ -2,7 +2,17 @@
 
 import z from 'zod';
 import { createRoomSchema } from '../schemas/rooms';
+import { getCurrentUser } from '../lib/getCurrentUser';
 
-export async function createRoom(
-  unsafeData: z.infer<typeof createRoomSchema>
-) {}
+export async function createRoom(unsafeData: z.infer<typeof createRoomSchema>) {
+  const { success, data } = createRoomSchema.safeParse(unsafeData);
+
+  if (!success) {
+    return { error: true, message: 'Invalud room data' };
+  }
+
+  const user = await getCurrentUser();
+  if (user == null) {
+    return { error: true, message: 'User not authenticated' };
+  }
+}
