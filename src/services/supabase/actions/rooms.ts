0296dc6e349a3f2b075ed66f278281e7b9da808c
Nader PJR <pjrnader@gmail.com>
2026-01-07 21:18:50 +0430

    rooms action

--- src/services/supabase/actions/rooms.ts
+++ src/services/supabase/actions/rooms.ts
@@ -3,6 +3,7 @@
 import z from 'zod';
 import { createRoomSchema } from '../schemas/rooms';
 import { getCurrentUser } from '../lib/getCurrentUser';
+import { createClient } from '../server';
 
 export async function createRoom(unsafeData: z.infer<typeof createRoomSchema>) {
   const { success, data } = createRoomSchema.safeParse(unsafeData);
@@ -15,4 +16,8 @@ export async function createRoom(unsafeData: z.infer<typeof createRoomSchema>) {
   if (user == null) {
     return { error: true, message: 'User not authenticated' };
   }
+
+  const supabase = await createClient();
+
+  supabase.from('chat_room');
 }
